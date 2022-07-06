7549d188ec0e6bde9ec928362234aa10422cdb1b
madisonlowe <mcourteney@live.com>
2022-07-06 21:12:38 +0100

    Updated link in data.js.

--- src/data.js
+++ src/data.js
@@ -37,10 +37,9 @@ export let projects = [
   },
   {
     title: "Auth0",
-    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque scelerisque tristique semper. Cras et nisl et orci ornare pulvinar. Vivamus sit amet velit viverra, vehicula ipsum a, dignissim ex.",
+    desc: "An experiment in using Auth0 to build a user login page.",
     src: "https://i.pinimg.com/564x/44/f9/49/44f949056f550359f76ea8cb42702444.jpg",
-    github:
-      "https://github.com/SchoolOfCode/w10d4_auth0-workshop-skan-son-butchers",
+    github: "https://github.com/madisonlowe/w10d4_auth0",
   },
 ];
 

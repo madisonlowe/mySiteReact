da2d179f81bf07b1d898deccd15ab373a9af07a7
madisonlowe <mcourteney@live.com>
2022-07-10 23:08:25 +0100

    Updated data.js and CSS.

--- src/data.js
+++ src/data.js
@@ -1,53 +1,54 @@
-export let projects = [
+export const projects = [
   {
     title: "Project Week",
     desc: "A full-stack application written in React, Node.js, Express and PostgreSQL. In our ninth week of learning how to code, me and three other teammates came together to build this MVP in under a week, before presenting our product to a panel of senior developers and industry professionals.",
-    src: "https://i.pinimg.com/564x/44/f9/49/44f949056f550359f76ea8cb42702444.jpg",
+    src: "https://cdn.discordapp.com/attachments/657862753687699467/995807925807358002/cropone.png",
     github: "https://github.com/SchoolOfCode/w9_frontend-project-room-21-1",
   },
   {
     title: "Site 1.0",
     desc: "The first version of my personal website, built in vanilla JavaScript. The research and design phases are documented further in the linked repository.",
-    src: "https://i.pinimg.com/564x/44/f9/49/44f949056f550359f76ea8cb42702444.jpg",
+    src: "https://cdn.discordapp.com/attachments/657862753687699467/995808330415087616/eyup23.png",
     github: "https://github.com/madisonlowe/mySiteVanilla",
   },
   {
     title: "Site 2.0",
     desc: "The second version of my personal website, build in React. Based off of the designs and feedback received regarding the first iteration in Site 1.0.",
-    src: "https://i.pinimg.com/564x/44/f9/49/44f949056f550359f76ea8cb42702444.jpg",
+    src: "https://cdn.discordapp.com/attachments/657862753687699467/995808695994818660/plan23.png",
     github: "https://github.com/madisonlowe/mySiteReact",
   },
   {
     title: "To Do List",
     desc: "A to-do list app, built in React.",
-    src: "https://i.pinimg.com/564x/44/f9/49/44f949056f550359f76ea8cb42702444.jpg",
+    src: "https://cdn.discordapp.com/attachments/657862753687699467/995810253407015002/todo2x3.png",
     github: "https://github.com/madisonlowe/toDoList",
   },
   {
     title: "Rock, Paper, Scissors!",
     desc: "The first game I ever built in JavaScript! A small browser game with light and dark mode toggle.",
-    src: "https://i.pinimg.com/564x/44/f9/49/44f949056f550359f76ea8cb42702444.jpg",
+    src: "https://cdn.discordapp.com/attachments/657862753687699467/995808945845309581/rps23.png",
     github: "https://github.com/madisonlowe/RPS",
   },
   {
     title: "Cat Treat Generator",
     desc: "A tiny microproject: a cat treat randomiser, stylised to look like a receipt at a continental eatery.",
-    src: "https://i.pinimg.com/564x/44/f9/49/44f949056f550359f76ea8cb42702444.jpg",
+    src: "https://cdn.discordapp.com/attachments/657862753687699467/995809689268924446/cafe23.png",
     github: "https://github.com/madisonlowe/catTreat",
   },
-  {
-    title: "Auth0",
-    desc: "An experiment in using Auth0 to build a user login page.",
-    src: "https://i.pinimg.com/564x/44/f9/49/44f949056f550359f76ea8cb42702444.jpg",
-    github: "https://github.com/madisonlowe/w10d4_auth0",
-  },
+  // {
+  //   title: "Auth0",
+  //   desc: "An experiment in using Auth0 to build a user login page.",
+  //   src: "https://cdn.discordapp.com/attachments/657862753687699467/995811199629410404/auth02x3.png",
+  //   github: "https://github.com/madisonlowe/w10d4_auth0",
+  // },
 ];
 
-export let descriptors = [
+export const descriptors = [
   "software developer",
   "lake swimmer",
   "Sheffielder",
   "houseplant enthusiast",
   "cat person",
-  "bad cook",
+  "questionable cook",
+  "bad dancer",
 ];

c90d0377ebc4e0eb170adcfdeb835315261579c8
madisonlowe <mcourteney@live.com>
2022-07-12 12:22:14 +0100

    Fixed assets/images folder imports, now work in data.js.

--- src/Components/ProjectCard/index.js
+++ src/Components/ProjectCard/index.js
@@ -3,7 +3,7 @@ import "./ProjectCard.css";
 function ProjectCard({ title, desc, src, github }) {
   return (
     <div className="card-div">
-      <img src={src} alt="" className="card-img" />
+      <img src={src} alt={title} className="card-img" />
       <div className="card-desc">
         <p>
           <b>{title}</b> / <a href={github}>Code</a>
--- src/data.js
+++ src/data.js
@@ -1,44 +1,52 @@
+// import authPic from "./assets/images/auth02x3.png";
+import cafePic from "./assets/images/cafe2x3.png";
+import eyupPic from "./assets/images/eyup2x3.png";
+import w9Pic from "./assets/images/nine2x3.png";
+import planPic from "./assets/images/plan2x3.png";
+import rpsPic from "./assets/images/rps2x3.png";
+import todoPic from "./assets/images/todo2x3.png";
+
 export const projects = [
   {
     title: "Project Week",
     desc: "A full-stack application written in React, Node.js, Express and PostgreSQL. In our ninth week of learning how to code, me and three other teammates came together to build this MVP in under a week, before presenting our product to a panel of senior developers and industry professionals.",
-    src: "https://cdn.discordapp.com/attachments/657862753687699467/995807925807358002/cropone.png",
+    src: w9Pic,
     github: "https://github.com/SchoolOfCode/w9_frontend-project-room-21-1",
   },
   {
     title: "Site 1.0",
     desc: "The first version of my personal website, built in vanilla JavaScript. The research and design phases are documented further in the linked repository.",
-    src: "https://cdn.discordapp.com/attachments/657862753687699467/995808330415087616/eyup23.png",
+    src: eyupPic,
     github: "https://github.com/madisonlowe/mySiteVanilla",
   },
   {
     title: "Site 2.0",
     desc: "The second version of my personal website, build in React. Based off of the designs and feedback received regarding the first iteration in Site 1.0.",
-    src: "https://cdn.discordapp.com/attachments/657862753687699467/995808695994818660/plan23.png",
+    src: planPic,
     github: "https://github.com/madisonlowe/mySiteReact",
   },
   {
     title: "To Do List",
     desc: "A to-do list app, built in React.",
-    src: "https://cdn.discordapp.com/attachments/657862753687699467/995810253407015002/todo2x3.png",
+    src: todoPic,
     github: "https://github.com/madisonlowe/toDoList",
   },
   {
     title: "Rock, Paper, Scissors!",
     desc: "The first game I ever built in JavaScript! A small browser game with light and dark mode toggle.",
-    src: "https://cdn.discordapp.com/attachments/657862753687699467/995808945845309581/rps23.png",
+    src: rpsPic,
     github: "https://github.com/madisonlowe/RPS",
   },
   {
     title: "Cat Treat Generator",
     desc: "A tiny microproject: a cat treat randomiser, stylised to look like a receipt at a continental eatery.",
-    src: "https://cdn.discordapp.com/attachments/657862753687699467/995809689268924446/cafe23.png",
+    src: cafePic,
     github: "https://github.com/madisonlowe/catTreat",
   },
   // {
   //   title: "Auth0",
   //   desc: "An experiment in using Auth0 to build a user login page.",
-  //   src: "https://cdn.discordapp.com/attachments/657862753687699467/995811199629410404/auth02x3.png",
+  //   src: authPic,
   //   github: "https://github.com/madisonlowe/w10d4_auth0",
   // },
 ];
